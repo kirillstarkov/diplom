f669aa01c64bdd262b1a4d996ecbf44b2081b9b5
kirillstarkov <xmdnssx@gmail.com>
2020-11-24 11:35:15 +0200

    2

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('.menu-opener').on('click', function(e) { // Наша бургер кнопка на которую мы будем нажимать
+		e.preventDefault();
+		$('body').toggleClass('burger') // body это селектор на который будет довешиваться класс mobile-menu-active при нажатии на бургер кнопку
+	})
+})
+
